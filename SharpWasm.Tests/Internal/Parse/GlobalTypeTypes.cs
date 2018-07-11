ddab5bb917200fb3ad8b612fb22e7a288f0c9224
Ole Martin Gjersvik <olem@olem.org>
2018-07-11 10:46:12 +0200

    GlobalType

---
+++ SharpWasm.Tests/Internal/Parse/GlobalTypeTypes.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using SharpWasm.Internal.Parse;
+using SharpWasm.Tests.Helpers;
+
+namespace SharpWasm.Tests.Internal.Parse
+{
+    [TestFixture]
+    public class GlobalTypeTypes
+    {
+        [Test]
+        public void ContentType()
+        {
+            var globalType = new GlobalType(ValueType.I32, false);
+            Assert.That(globalType.ContentType, Is.EqualTo(ValueType.I32));
+        }
+
+        [Test]
+        public void Mutability()
+        {
+            var globalType = new GlobalType(ValueType.I32, false);
+            Assert.That(globalType.Mutability, Is.False);
+        }
+
+        [Test]
+        public void Parse()
+        {
+            const string hex = "7F00";
+            using (var reader = BinaryTools.HexToReader(hex))
+            {
+                var globalType = new GlobalType(reader);
+                Assert.That(globalType.ContentType, Is.EqualTo(ValueType.I32));
+                Assert.That(globalType.Mutability, Is.False);
+            }
+        }
+    }
+}
---
+++ SharpWasm/Internal/Parse/GlobalType.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace SharpWasm.Internal.Parse
+{
+    internal class GlobalType
+    {
+        public readonly ValueType ContentType;
+        public readonly bool Mutability;
+
+        public GlobalType(BinaryReader reader)
+        {
+            ContentType = VarIntSigned.ToValueType(reader);
+            Mutability = VarIntUnsigned.ToBool(reader);
+        }
+
+        public GlobalType(ValueType contentType, bool mutability)
+        {
+            ContentType = contentType;
+            Mutability = mutability;
+        }
+    }
+}
