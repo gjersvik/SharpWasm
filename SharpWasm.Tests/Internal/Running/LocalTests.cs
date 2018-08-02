cd58d9fa68fb2e36b4fc81fa728cabe53f9627ee
Ole Martin Gjersvik <OLGJ@statoil.com>
2018-08-02 17:24:25 +0200

    Test for stack

---
+++ SharpWasm.Tests/Internal/Running/LocalTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using SharpWasm.Internal.Running;
+
+namespace SharpWasm.Tests.Internal.Running
+{
+    [TestFixture]
+    public class LocalTests
+    {
+        [Test]
+        public void Constructor()
+        {
+            var stack = new Stack();
+            var local = new Local(stack);
+            Assert.That(local.Stack, Is.EqualTo(stack));
+        }
+    }
+}
---
+++ SharpWasm.Tests/Internal/Running/StackTest.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using SharpWasm.Internal.Running;
+
+namespace SharpWasm.Tests.Internal.Running
+{
+    [TestFixture]
+    public class StackTest
+    {
+        [Test]
+        public void PushAndPopAll()
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2L);
+            stack.Push(3F);
+            stack.Push(4.0);
+            Assert.That(stack.PopDouble(),Is.EqualTo(4) );
+            Assert.That(stack.PopFloat(), Is.EqualTo(3));
+            Assert.That(stack.PopLong(), Is.EqualTo(2));
+            Assert.That(stack.PopInt(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void PopIntWrongType()
+        {
+            var stack = new Stack();
+            stack.Push(2L);
+            Assert.That(() => stack.PopInt(), Throws.Exception.Message.Contain("I64"));
+        }
+
+        [Test]
+        public void PopLongWrongType()
+        {
+            var stack = new Stack();
+            stack.Push(3F);
+            Assert.That(() => stack.PopLong(), Throws.Exception.Message.Contain("F32"));
+        }
+
+        [Test]
+        public void PopFloatWrongType()
+        {
+            var stack = new Stack();
+            stack.Push(4.0);
+            Assert.That(() => stack.PopFloat(), Throws.Exception.Message.Contain("F64"));
+        }
+
+        [Test]
+        public void PopDoubleWrongType()
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            Assert.That(() => stack.PopDouble(), Throws.Exception.Message.Contain("I32"));
+        }
+
+        [Test]
+        public void StackOverflow()
+        {
+            var stack = new Stack(2);
+            stack.Push(1);
+            stack.Push(1);
+            Assert.That(() => stack.Push(1), Throws.InstanceOf<WebAssemblyStackOverflowException>());
+        }
+    }
+}
