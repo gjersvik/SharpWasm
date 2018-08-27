dc474ba6f881fd03a14b6789699522612d0a63b8
Ole Martin Gjersvik <olgj@equinor.com>
2018-08-27 14:28:14 +0200

    Result type

---
+++ SharpWasm.Tests/Internal/Runtime/ResultTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using SharpWasm.Internal.Runtime;
+
+namespace SharpWasm.Tests.Internal.Runtime
+{
+    [TestFixture]
+    public class ResultTests
+    {
+        [Test]
+        public void ConstructorEmpty()
+        {
+            var result = new Result();
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.Value, Is.Null);
+                Assert.That(result.Trap, Is.Null);
+            });
+        }
+
+        [Test]
+        public void ConstructorValue()
+        {
+            var result = new Result(42);
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.Value, Is.Not.Null);
+                Assert.That(result.Trap, Is.Null);
+            });
+        }
+
+        [Test]
+        public void ConstructorTrap()
+        {
+            var result = new Result(new Trap());
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.Value, Is.Null);
+                Assert.That(result.Trap, Is.Not.Null);
+            });
+        }
+
+        [Test]
+        public void CastValue()
+        {
+            Result result = (Value)42;
+            var value = result.Value ?? 0;
+            Assert.That((int)value, Is.EqualTo(42));
+        }
+
+        [Test]
+        public void CastTrap()
+        {
+            Result result = new Trap();
+            Assert.That(result.Trap, Is.Not.Null);
+        }
+    }
+}
---
+++ SharpWasm/Internal/Runtime/Result.cs
@@ -0,0 +1,34 @@
+﻿using JetBrains.Annotations;
+
+namespace SharpWasm.Internal.Runtime
+{
+    internal class Result
+    {
+        public readonly Value? Value;
+        [CanBeNull] public readonly Trap Trap;
+
+        public Result() : this(null, null)
+        {
+
+        }
+
+        public Result(Value trap) : this(trap, null)
+        {
+
+        }
+
+        public Result(Trap trap) : this(null, trap)
+        {
+
+        }
+
+        private Result(Value? value, Trap trap)
+        {
+            Value = value;
+            Trap = trap;
+        }
+
+        public static implicit operator Result(Value value) => new Result(value);
+        public static implicit operator Result(Trap trap) => new Result(trap);
+    }
+}
