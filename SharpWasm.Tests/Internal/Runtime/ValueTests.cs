de1ecf78f9ec47dc93090991f13391c199df8e44
Ole Martin Gjersvik <olgj@equinor.com>
2018-08-27 11:08:55 +0200

    Value object

---
+++ SharpWasm.Tests/Internal/Runtime/ValueTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using NUnit.Framework;
+using SharpWasm.Internal.Runtime;
+using ValueType = SharpWasm.Internal.Parse.Types.ValueType;
+
+namespace SharpWasm.Tests.Internal.Runtime
+{
+    [TestFixture]
+    public class ValueTests
+    {
+        [Test]
+        public void ConstructorInt()
+        {
+            var value = new Value(-42);
+            Assert.Multiple(() =>
+            {
+                Assert.That(value.Type, Is.EqualTo(ValueType.I32));
+                Assert.That((int)value, Is.EqualTo(-42).And.TypeOf<int>());
+            });
+        }
+
+        [Test]
+        public void ConstructorUInt()
+        {
+            var value = new Value(42U);
+            Assert.Multiple(() =>
+            {
+                Assert.That(value.Type, Is.EqualTo(ValueType.I32));
+                Assert.That((uint)value, Is.EqualTo(42).And.TypeOf<uint>());
+            });
+        }
+
+        [Test]
+        public void ConstructorLong()
+        {
+            var value = new Value(-42L);
+            Assert.Multiple(() =>
+            {
+                Assert.That(value.Type, Is.EqualTo(ValueType.I64));
+                Assert.That((long)value, Is.EqualTo(-42).And.TypeOf<long>());
+            });
+        }
+
+        [Test]
+        public void ConstructorULong()
+        {
+            var value = new Value(42UL);
+            Assert.Multiple(() =>
+            {
+                Assert.That(value.Type, Is.EqualTo(ValueType.I64));
+                Assert.That((ulong)value, Is.EqualTo(42).And.TypeOf<ulong>());
+            });
+        }
+
+        [Test]
+        public void ConstructorFloat()
+        {
+            var value = new Value(42.0F);
+            Assert.Multiple(() =>
+            {
+                Assert.That(value.Type, Is.EqualTo(ValueType.F32));
+                Assert.That((float)value, Is.EqualTo(42).And.TypeOf<float>());
+            });
+        }
+
+        [Test]
+        public void ConstructorDouble()
+        {
+            var value = new Value(42.0D);
+            Assert.Multiple(() =>
+            {
+                Assert.That(value.Type, Is.EqualTo(ValueType.F64));
+                Assert.That((double)value, Is.EqualTo(42).And.TypeOf<double>());
+            });
+        }
+
+        [Test]
+        public void WrongCastUInt()
+        {
+            Value value = 1.0;
+            Assert.That(() => (uint)value, Throws.TypeOf<InvalidCastException>());
+        }
+
+        [Test]
+        public void WrongCastULong()
+        {
+            Value value = 1.0;
+            Assert.That(() => (ulong)value, Throws.TypeOf<InvalidCastException>());
+        }
+
+        [Test]
+        public void CastUInt()
+        {
+            Value value = 43U;
+            Assert.That(value.Type, Is.EqualTo(ValueType.I32));
+        }
+
+        [Test]
+        public void CastULong()
+        {
+            Value value = 43UL;
+            Assert.That(value.Type, Is.EqualTo(ValueType.I64));
+        }
+    }
+}
--- SharpWasm/Internal/Runtime/Stack.cs
+++ SharpWasm/Internal/Runtime/Stack.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace SharpWasm.Internal.Runtime
 {
@@ -7,36 +6,17 @@ namespace SharpWasm.Internal.Runtime
     {
         public Stack(int stackSize = 10000)
         {
-            _stack = new Stack<IStackValue>(stackSize);
+            _stack = new Stack<Value>(stackSize);
             _maxStack = stackSize;
         }
 
-        public void Push(int value)
-        {
-            Push(new StackI32(value));
-        }
 
-        public void Push(long value)
-        {
-            Push(new StackI64(value));
-        }
-
-        public void Push(float value)
-        {
-            Push(new StackF32(value));
-        }
-
-        public void Push(double value)
-        {
-            Push(new StackF64(value));
-        }
-
-        public IStackValue Pop()
+        public Value Pop()
         {
             return _stack.Pop();
         }
 
-        public void Push(IStackValue stackValue)
+        public void Push(Value stackValue)
         {
             if (_stack.Count >= _maxStack) throw new WebAssemblyStackOverflowException();
             _stack.Push(stackValue);
@@ -44,84 +24,27 @@ namespace SharpWasm.Internal.Runtime
 
         public int PopInt()
         {
-            var raw = Pop();
-            if (raw is StackI32 value) return value.Value;
-            throw new WebAssemblyRuntimeException($"Wrong type on top off stack. Expected I32 found {raw.Type}.");
+            return (int) Pop();
         }
 
         public long PopLong()
         {
-            var raw = Pop();
-            if (raw is StackI64 value) return value.Value;
-            throw new WebAssemblyRuntimeException($"Wrong type on top off stack. Expected I64 found {raw.Type}.");
+            return (long) Pop();
         }
 
         public float PopFloat()
         {
-            var raw = Pop();
-            if (raw is StackF32 value) return value.Value;
-            throw new WebAssemblyRuntimeException($"Wrong type on top off stack. Expected F32 found {raw.Type}.");
+            return (float) Pop();
         }
 
         public double PopDouble()
         {
-            var raw = Pop();
-            if (raw is StackF64 value) return value.Value;
-            throw new WebAssemblyRuntimeException($"Wrong type on top off stack. Expected F64 found {raw.Type}.");
+            return (double) Pop();
         }
 
         public int Count => _stack.Count;
 
-        private readonly Stack<IStackValue> _stack;
+        private readonly Stack<Value> _stack;
         private readonly int _maxStack;
-    }
-
-    public interface IStackValue
-    {
-        ValueType Type { get; }
-    }
-
-    public class StackI32 : IStackValue
-    {
-        public ValueType Type { get; } = Parse.Types.ValueType.I32;
-        public readonly int Value;
-
-        public StackI32(int value)
-        {
-            Value = value;
-        }
-    }
-
-    public class StackI64 : IStackValue
-    {
-        public ValueType Type { get; } = Parse.Types.ValueType.I64;
-        public readonly long Value;
-
-        public StackI64(long value)
-        {
-            Value = value;
-        }
-    }
-
-    public class StackF32 : IStackValue
-    {
-        public ValueType Type { get; } = Parse.Types.ValueType.F32;
-        public readonly float Value;
-
-        public StackF32(float value)
-        {
-            Value = value;
-        }
-    }
-
-    public class StackF64 : IStackValue
-    {
-        public ValueType Type { get; } = Parse.Types.ValueType.F64;
-        public readonly double Value;
-
-        public StackF64(double value)
-        {
-            Value = value;
-        }
     }
 }
