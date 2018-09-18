2c71a12c817a6b8afcfc4811831bce45a1e35377
Ole Martin Gjersvik <olgj@equinor.com>
2018-09-18 17:37:33 +0200

    Add Function to Core

--- SharpWasm.Core/Segments/Function.cs
+++ SharpWasm.Core/Segments/Function.cs
@@ -1,6 +1,58 @@
-﻿namespace SharpWasm.Core.Segments
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using SharpWasm.Core.Code;
+using ValueType = SharpWasm.Core.Types.ValueType;
+
+namespace SharpWasm.Core.Segments
 {
-    internal class Function
+    internal class Function:IEquatable<Function>
     {
+        public readonly uint TypeIndex;
+        public readonly ImmutableArray<ValueType> Locals;
+        public readonly ImmutableArray<IInstruction> Body;
+
+        public Function(uint typeIndex, IEnumerable<ValueType> locals, IEnumerable<IInstruction> body)
+        {
+            TypeIndex = typeIndex;
+            Locals = locals.ToImmutableArray();
+            Body = body.ToImmutableArray();
+        }
+
+        public bool Equals(Function other)
+        {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return TypeIndex == other.TypeIndex && Locals.SequenceEqual(other.Locals) && Body.SequenceEqual(other.Body);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((Function) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (int) TypeIndex;
+                hashCode = (hashCode * 397) ^ Locals.Length.GetHashCode();
+                hashCode = (hashCode * 397) ^ Body.Length.GetHashCode();
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(Function left, Function right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Function left, Function right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
--- SharpWasm.Tests/Core/Segments/FunctionTests.cs
+++ SharpWasm.Tests/Core/Segments/FunctionTests.cs
@@ -1,5 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Immutable;
+using NUnit.Framework;
+using SharpWasm.Core.Code;
 using SharpWasm.Core.Segments;
+using SharpWasm.Core.Types;
 
 namespace SharpWasm.Tests.Core.Segments
 {
@@ -9,7 +12,29 @@ namespace SharpWasm.Tests.Core.Segments
         [Test]
         public void Constructor()
         {
-            Assert.That(() => new Function(), Throws.Nothing);
+            var function = new Function(0, new[] {ValueType.I32}, new IInstruction[] {Instruction.I32Const(0), Instruction.End});
+            Assert.Multiple(() =>
+            {
+                Assert.That(function.TypeIndex, Is.EqualTo(0), "TypeIndex");
+                Assert.That(function.Locals, Is.EqualTo(new[] { ValueType.I32 }).AsCollection, "Locals");
+                Assert.That(function.Body, Is.EqualTo(new IInstruction[] { Instruction.I32Const(0), Instruction.End }).AsCollection, "Body");
+            });
+        }
+
+        [Test]
+        public void Equals()
+        {
+            var a = new Function(0, ImmutableArray<ValueType>.Empty, ImmutableArray<IInstruction>.Empty);
+            var b = new Function(0, ImmutableArray<ValueType>.Empty, ImmutableArray<IInstruction>.Empty);
+            Assert.That(a.Equals(a), Is.True);
+            Assert.That(a.Equals(b), Is.True);
+            Assert.That(a.Equals(null), Is.False);
+            Assert.That(a.Equals((object)a), Is.True);
+            Assert.That(a.Equals((object)b), Is.True);
+            Assert.That(a.Equals((object)null), Is.False);
+            Assert.That(a.GetHashCode(), Is.EqualTo(b.GetHashCode()));
+            Assert.That(a == b, Is.True);
+            Assert.That(a != b, Is.False);
         }
     }
 }
