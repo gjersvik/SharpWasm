15fa68fa36b6aad4b91653a3344a61dc9f694843
Ole Martin Gjersvik <olgj@equinor.com>
2018-09-18 17:51:14 +0200

    Created Global in core

--- SharpWasm.Core/Segments/Global.cs
+++ SharpWasm.Core/Segments/Global.cs
@@ -1,6 +1,54 @@
-﻿namespace SharpWasm.Core.Segments
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using JetBrains.Annotations;
+using SharpWasm.Core.Code;
+using SharpWasm.Core.Types;
+
+namespace SharpWasm.Core.Segments
 {
-    internal class Global
+    internal class Global: IEquatable<Global>
     {
+        [NotNull]public readonly GlobalType Type;
+        public readonly ImmutableArray<IInstruction> Init;
+
+        public Global(GlobalType type, IEnumerable<IInstruction> init)
+        {
+            Type = type;
+            Init = init.ToImmutableArray();
+        }
+
+        public bool Equals(Global other)
+        {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Type.Equals(other.Type) && Init.SequenceEqual(other.Init);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((Global) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Type.GetHashCode() * 397) ^ Init.FirstOrDefault()?.GetHashCode() ?? 0;
+            }
+        }
+
+        public static bool operator ==(Global left, Global right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Global left, Global right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
--- SharpWasm.Tests/Core/Segments/GlobalTests.cs
+++ SharpWasm.Tests/Core/Segments/GlobalTests.cs
@@ -1,5 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Immutable;
+using NUnit.Framework;
+using SharpWasm.Core.Code;
 using SharpWasm.Core.Segments;
+using SharpWasm.Core.Types;
 
 namespace SharpWasm.Tests.Core.Segments
 {
@@ -9,7 +12,28 @@ namespace SharpWasm.Tests.Core.Segments
         [Test]
         public void Constructor()
         {
-            Assert.That(() => new Global(), Throws.Nothing);
+            var global = new Global(new GlobalType(ValueType.F32, false), new IInstruction[] { Instruction.I32Const(0), Instruction.End });
+            Assert.Multiple(() =>
+            {
+                Assert.That(global.Type, Is.EqualTo(new GlobalType(ValueType.F32, false)).AsCollection, "Type");
+                Assert.That(global.Init, Is.EqualTo(new IInstruction[] { Instruction.I32Const(0), Instruction.End }).AsCollection, "Init");
+            });
+        }
+
+        [Test]
+        public void Equals()
+        {
+            var a = new Global(new GlobalType(ValueType.F32, false), ImmutableArray<IInstruction>.Empty);
+            var b = new Global(new GlobalType(ValueType.F32, false), ImmutableArray<IInstruction>.Empty);
+            Assert.That(a.Equals(a), Is.True);
+            Assert.That(a.Equals(b), Is.True);
+            Assert.That(a.Equals(null), Is.False);
+            Assert.That(a.Equals((object)a), Is.True);
+            Assert.That(a.Equals((object)b), Is.True);
+            Assert.That(a.Equals((object)null), Is.False);
+            Assert.That(a.GetHashCode(), Is.EqualTo(b.GetHashCode()));
+            Assert.That(a == b, Is.True);
+            Assert.That(a != b, Is.False);
         }
     }
 }
