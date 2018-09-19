34e2cd636dce1bec83f8529739f19758b27d211e
Ole Martin Gjersvik <olem@olem.org>
2018-09-19 06:49:36 +0200

    Created Element Segment

--- SharpWasm.Core/Segments/Element.cs
+++ SharpWasm.Core/Segments/Element.cs
@@ -1,6 +1,57 @@
-﻿namespace SharpWasm.Core.Segments
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using SharpWasm.Core.Code;
+
+namespace SharpWasm.Core.Segments
 {
-    internal class Element
+    internal class Element: IEquatable<Element>
     {
+        public readonly uint Table;
+        public readonly ImmutableArray<IInstruction> Offset;
+        public readonly ImmutableArray<uint> Init;
+
+        public Element(uint table, IEnumerable<IInstruction> offset, IEnumerable<uint> init)
+        {
+            Table = table;
+            Offset = offset.ToImmutableArray();
+            Init = init.ToImmutableArray();
+        }
+
+        public bool Equals(Element other)
+        {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Table == other.Table && Offset.SequenceEqual(other.Offset) && Init.SequenceEqual(other.Init);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((Element) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (int) Table;
+                hashCode = (hashCode * 397) ^ Offset.FirstOrDefault()?.GetHashCode() ?? 0;
+                hashCode = (hashCode * 397) ^ Init.Length.GetHashCode();
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(Element left, Element right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Element left, Element right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
--- SharpWasm.Tests/Core/Segments/ElementTests.cs
+++ SharpWasm.Tests/Core/Segments/ElementTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Immutable;
+using NUnit.Framework;
+using SharpWasm.Core.Code;
 using SharpWasm.Core.Segments;
 
 namespace SharpWasm.Tests.Core.Segments
@@ -9,7 +11,29 @@ namespace SharpWasm.Tests.Core.Segments
         [Test]
         public void Constructor()
         {
-            Assert.That(() => new Element(), Throws.Nothing);
+            var global = new Element(0, new IInstruction[] { Instruction.I32Const(3), Instruction.End }, new uint[] { 1, 2, 3 });
+            Assert.Multiple(() =>
+            {
+                Assert.That(global.Table, Is.EqualTo(0).AsCollection, "Table");
+                Assert.That(global.Offset, Is.EqualTo(new IInstruction[] { Instruction.I32Const(3), Instruction.End }).AsCollection, "Offset");
+                Assert.That(global.Init, Is.EqualTo(new uint[] { 1, 2, 3 }).AsCollection, "Init");
+            });
+        }
+
+        [Test]
+        public void Equals()
+        {
+            var a = new Element(0,ImmutableArray<IInstruction>.Empty, new uint[]{1,2,3});
+            var b = new Element(0, ImmutableArray<IInstruction>.Empty, new uint[] { 1, 2, 3 });
+            Assert.That(a.Equals(a), Is.True);
+            Assert.That(a.Equals(b), Is.True);
+            Assert.That(a.Equals(null), Is.False);
+            Assert.That(a.Equals((object)a), Is.True);
+            Assert.That(a.Equals((object)b), Is.True);
+            Assert.That(a.Equals((object)null), Is.False);
+            Assert.That(a.GetHashCode(), Is.EqualTo(b.GetHashCode()));
+            Assert.That(a == b, Is.True);
+            Assert.That(a != b, Is.False);
         }
     }
 }
