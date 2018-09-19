e526aa186b41782c7666445d39923cacc23a881f
Ole Martin Gjersvik <olem@olem.org>
2018-09-19 09:32:38 +0200

    Created Import Segment

--- SharpWasm.Core/Segments/Import.cs
+++ SharpWasm.Core/Segments/Import.cs
@@ -1,6 +1,104 @@
-﻿namespace SharpWasm.Core.Segments
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+using SharpWasm.Core.Types;
+
+namespace SharpWasm.Core.Segments
 {
-    internal class Import
+    internal class Import: IEquatable<Import>
     {
+        [NotNull]public readonly string Module;
+        [NotNull] public readonly string Name;
+        public readonly ExternalKind Type;
+        [CanBeNull] public readonly uint? Function;
+        [CanBeNull] public readonly TableType Table;
+        [CanBeNull] public readonly MemoryType Memory;
+        [CanBeNull] public readonly GlobalType Global;
+
+        public Import(string module, string name, uint functionType)
+        {
+            Module = module;
+            Name = name;
+            Type = ExternalKind.Function;
+            Function = functionType;
+        }
+
+        public Import(string module, string name, [NotNull]TableType tableType)
+        {
+            Module = module;
+            Name = name;
+            Type = ExternalKind.Table;
+            Table = tableType;
+        }
+
+        public Import(string module, string name, [NotNull]MemoryType memoryType)
+        {
+            Module = module;
+            Name = name;
+            Type = ExternalKind.Memory;
+            Memory = memoryType;
+        }
+
+        public Import(string module, string name, [NotNull]GlobalType globalType)
+        {
+            Module = module;
+            Name = name;
+            Type = ExternalKind.Global;
+            Global = globalType;
+        }
+
+        [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
+        public bool Equals(Import other)
+        {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+
+            if (!string.Equals(Module, other.Module)) return false;
+            if (!string.Equals(Name, other.Name)) return false;
+            if (Type != other.Type) return false;
+            // ReSharper disable once ConvertIfStatementToSwitchStatement
+            if (Type == ExternalKind.Function)
+                return Function.Equals(other.Function);
+            if (Type == ExternalKind.Table)
+                return Table.Equals(other.Table);
+            return Type == ExternalKind.Memory ? Memory.Equals(other.Memory) : Global.Equals(other.Global);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((Import) obj);
+        }
+
+        [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Module.GetHashCode();
+                hashCode = (hashCode * 397) ^ Name.GetHashCode();
+                hashCode = (hashCode * 397) ^ (int) Type;
+                // ReSharper disable once ConvertIfStatementToSwitchStatement
+                if (Type == ExternalKind.Function)
+                    return (hashCode * 397) ^ Function.GetHashCode();
+                if (Type == ExternalKind.Table)
+                    return (hashCode * 397) ^ Table.GetHashCode();
+                if (Type == ExternalKind.Memory)
+                    return (hashCode * 397) ^ Memory.GetHashCode();
+
+                return (hashCode * 397) ^ Global.GetHashCode();
+            }
+        }
+
+        public static bool operator ==(Import left, Import right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Import left, Import right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
--- SharpWasm.Tests/Core/Segments/ImportTests.cs
+++ SharpWasm.Tests/Core/Segments/ImportTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using SharpWasm.Core.Segments;
+using SharpWasm.Core.Types;
 
 namespace SharpWasm.Tests.Core.Segments
 {
@@ -7,9 +8,108 @@ namespace SharpWasm.Tests.Core.Segments
     public class ImportTests
     {
         [Test]
-        public void Constructor()
+        public void Function()
         {
-            Assert.That(() => new Import(), Throws.Nothing);
+            var import = new Import("module", "name",0);
+            Assert.Multiple(() =>
+            {
+                Assert.That(import.Module, Is.EqualTo("module"));
+                Assert.That(import.Name, Is.EqualTo("name"));
+                Assert.That(import.Type, Is.EqualTo(ExternalKind.Function));
+                Assert.That(import.Function, Is.EqualTo(0));
+            });
+        }
+
+        [Test]
+        public void TableType()
+        {
+            var import = new Import("module", "name", new TableType(0));
+            Assert.Multiple(() =>
+            {
+                Assert.That(import.Module, Is.EqualTo("module"));
+                Assert.That(import.Name, Is.EqualTo("name"));
+                Assert.That(import.Type, Is.EqualTo(ExternalKind.Table));
+                Assert.That(import.Table, Is.EqualTo(new TableType(0)));
+            });
+        }
+
+        [Test]
+        public void MemoryType()
+        {
+            var import = new Import("module", "name", new MemoryType(0));
+            Assert.Multiple(() =>
+            {
+                Assert.That(import.Module, Is.EqualTo("module"));
+                Assert.That(import.Name, Is.EqualTo("name"));
+                Assert.That(import.Type, Is.EqualTo(ExternalKind.Memory));
+                Assert.That(import.Memory, Is.EqualTo(new MemoryType(0)));
+            });
+        }
+
+        [Test]
+        public void GlobalType()
+        {
+            var import = new Import("module", "name", new GlobalType(ValueType.I32, false));
+            Assert.Multiple(() =>
+            {
+                Assert.That(import.Module, Is.EqualTo("module"));
+                Assert.That(import.Name, Is.EqualTo("name"));
+                Assert.That(import.Type, Is.EqualTo(ExternalKind.Global));
+                Assert.That(import.Global, Is.EqualTo(new GlobalType(ValueType.I32, false)));
+            });
+        }
+
+        [Test]
+        public void FunctionTypeEquals() =>
+            Equals(new Import("module", "name", 0), new Import("module", "name", 0));
+
+        [Test]
+        public void TableTypeEquals() =>
+            Equals(new Import("module", "name", new TableType(0)), new Import("module", "name", new TableType(0)));
+
+        [Test]
+        public void MemoryTypeEquals() =>
+            Equals(new Import("module", "name", new MemoryType(0)), new Import("module", "name", new MemoryType(0)));
+
+        [Test]
+        public void GlobalTypeEquals() =>
+            Equals(new Import("module", "name", new GlobalType(ValueType.I32, false)), new Import("module", "name", new GlobalType(ValueType.I32, false)));
+
+        [Test]
+        public void DifferentModuleNotEqual()
+        {
+            var a = new Import("module", "name", 0);
+            var b = new Import("otherModule", "name", 0);
+            Assert.That(a.Equals(b), Is.False);
+        }
+
+        [Test]
+        public void DifferentNameNotEqual()
+        {
+            var a = new Import("module", "name", 0);
+            var b = new Import("module", "otherName", 0);
+            Assert.That(a.Equals(b), Is.False);
+        }
+
+        [Test]
+        public void DifferentTypeNotEqual()
+        {
+            var a = new Import("module", "name", 0);
+            var b = new Import("module", "name", new TableType(0));
+            Assert.That(a.Equals(b), Is.False);
+        }
+
+        private static void Equals(Import a, Import b)
+        {
+            Assert.That(a.Equals(a), Is.True);
+            Assert.That(a.Equals(b), Is.True);
+            Assert.That(a.Equals(null), Is.False);
+            Assert.That(a.Equals((object)a), Is.True);
+            Assert.That(a.Equals((object)b), Is.True);
+            Assert.That(a.Equals((object)null), Is.False);
+            Assert.That(a.GetHashCode(), Is.EqualTo(b.GetHashCode()));
+            Assert.That(a == b, Is.True);
+            Assert.That(a != b, Is.False);
         }
     }
 }
