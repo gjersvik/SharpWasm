d50f38f0ae6b78310d13dd5e575991f844e6357c
Ole Martin Gjersvik <olem@olem.org>
2018-09-19 07:16:14 +0200

    Created Exsport Segment

--- SharpWasm.Core/Segments/Export.cs
+++ SharpWasm.Core/Segments/Export.cs
@@ -1,6 +1,55 @@
-﻿namespace SharpWasm.Core.Segments
+﻿using System;
+using JetBrains.Annotations;
+using SharpWasm.Core.Types;
+
+namespace SharpWasm.Core.Segments
 {
-    internal class Export
+    internal class Export: IEquatable<Export>
     {
+        [NotNull]public readonly string Name;
+        public readonly ExternalKind Type;
+        public readonly uint Index;
+
+        public Export(string name, ExternalKind type, uint index)
+        {
+            Name = name;
+            Type = type;
+            Index = index;
+        }
+
+        public bool Equals(Export other)
+        {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return string.Equals(Name, other.Name) && Type == other.Type && Index == other.Index;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((Export) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Name.GetHashCode();
+                hashCode = (hashCode * 397) ^ (int) Type;
+                hashCode = (hashCode * 397) ^ (int) Index;
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(Export left, Export right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Export left, Export right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
--- SharpWasm.Tests/Core/Segments/ExportTests.cs
+++ SharpWasm.Tests/Core/Segments/ExportTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using SharpWasm.Core.Segments;
+using SharpWasm.Core.Types;
 
 namespace SharpWasm.Tests.Core.Segments
 {
@@ -9,7 +10,29 @@ namespace SharpWasm.Tests.Core.Segments
         [Test]
         public void Constructor()
         {
-            Assert.That(() => new Export(), Throws.Nothing);
+            var export = new Export("test", ExternalKind.Function, 2);
+            Assert.Multiple(() =>
+            {
+                Assert.That(export.Name, Is.EqualTo("test").AsCollection, "Name");
+                Assert.That(export.Type, Is.EqualTo(ExternalKind.Function).AsCollection, "Type");
+                Assert.That(export.Index, Is.EqualTo(2).AsCollection, "Index");
+            });
+        }
+
+        [Test]
+        public void Equals()
+        {
+            var a = new Export("test", ExternalKind.Function, 2);
+            var b = new Export("test", ExternalKind.Function, 2);
+            Assert.That(a.Equals(a), Is.True);
+            Assert.That(a.Equals(b), Is.True);
+            Assert.That(a.Equals(null), Is.False);
+            Assert.That(a.Equals((object)a), Is.True);
+            Assert.That(a.Equals((object)b), Is.True);
+            Assert.That(a.Equals((object)null), Is.False);
+            Assert.That(a.GetHashCode(), Is.EqualTo(b.GetHashCode()));
+            Assert.That(a == b, Is.True);
+            Assert.That(a != b, Is.False);
         }
     }
 }
